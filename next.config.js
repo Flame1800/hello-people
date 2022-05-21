c79291a98c07d5dd93880f6cd1eac6d260df44fe
r.shaficoff <ruslan08022001@gmail.com>
2022-05-21 16:41:36 +0500

    change env

--- next.config.js
+++ next.config.js
@@ -1,8 +1,14 @@
 /** @type {import('next').NextConfig} */
+
+const baseUrl =
+    process.env.APP_ENV === 'dev'
+        ? "http://185.185.69.74:1337"
+        : "https://surgut.expert"
+
 const nextConfig = {
     reactStrictMode: true,
     env: {
-        SERVER_URL_PROD: `http://185.185.69.74:1337`
+        SERVER_URL_PROD: baseUrl,
     },
     async redirects() {
         return [
